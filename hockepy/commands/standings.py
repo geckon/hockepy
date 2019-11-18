c483bb5737e33b210272d7581aa089f8e5b00fbf
Tomáš Heger <heger.tomas@gmail.com>
2019-11-18 23:46:27 +0100

    Add skeleton for the new command. (refs #14)

---
+++ hockepy/commands/standings.py
@@ -0,0 +1,46 @@
+# vim: set fileencoding=utf-8 :
+
+#  ____  ____    ____      ______  ___  ____   _________  ______    ____  ____
+# |_   ||   _| .'    `.  .' ___  ||_  ||_  _| |_   ___  ||_   __ \ |_  _||_  _|
+#   | |__| |  /  .--.  \/ .'   \_|  | |_/ /     | |_  \_|  | |__) |  \ \  / /
+#   |  __  |  | |    | || |         |  __'.     |  _|  _   |  ___/    \ \/ /
+#  _| |  | |_ \  `--'  /\ `.___.'\ _| |  \ \_  _| |___/ | _| |_       _|  |_
+# |____||____| `.____.'  `._____.'|____||____||_________||_____|     |______|
+#
+
+"""
+hockepy.commands.standings
+-----------------------
+
+This module defines class for standings command.
+
+The purpose of this command is to retrieve and print the current
+standings as a table.
+"""
+
+import logging
+
+from hockepy.commands import BaseCommand
+
+
+class Standings(BaseCommand):
+    """Standings command.
+
+    Accepts the following arguments:
+    """
+
+    _COMMAND = 'standings'
+
+    @property
+    def description(self):
+        """Return the command's short description for user."""
+        return 'Print the current standings.'
+
+    @classmethod
+    def register_parser(cls, subparsers):
+        """Register and return the sub-command's parser."""
+        pass
+
+    def run(self):
+        """Run the command with the given arguments."""
+        logging.debug('Running the %r command.', self.command)
